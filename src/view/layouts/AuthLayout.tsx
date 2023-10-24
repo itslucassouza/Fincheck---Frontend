3c2816ced307dc3e8cde357826c7103977eb8043
itslucassouza <lucasvini193@hotmail.com>
2023-10-24 20:28:28 -0300

    add new icons theme

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,11 +1,12 @@
 import Ilustration from '../../assets/ilustration.png'
+import Logo from '../../assets/logo.svg'
 
 export function AuthLayout() {
     return (
         <div className="flex w-full h-full">
-            <div className=" w-1/2" >
+            {/* <div className=" w-1/2" >
                 
-            </div>
+            </div> */}
             <div 
             className="
                 w-1/2 
@@ -22,7 +23,10 @@ export function AuthLayout() {
                     select-none
                 " />
 
-                <div className="max-w[656px] bg-white bottom-8  p-10 absolute rounded-b-[32px]">
+                <div className="max-w[656px]
+                flex flex-col gap-6
+                bg-white bottom-8  p-10 absolute rounded-b-[32px]">
+                    <img src={Logo}  />
                   <p className='text-gray-700 font-medium text-xl'>
                     Gerencie suas finanças pessoais de forma simples com o fincheck, e o melhor, 
                     totalmente de graça
